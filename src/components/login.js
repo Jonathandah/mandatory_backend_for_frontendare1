26a3b75ac8214392dc1d2b987b5fea45efa0e035
Jonathan Dahnberg <Jonathan.MeeprongDahnberg@utb.ecutbildning.se>
2019-06-05 11:10:47 +0200

    register route added to server and omptimized uservalidation

--- src/components/login.js
+++ src/components/login.js
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 import { login$, updateUserLogin } from "../store/storeLogin";
 import { updateUser } from "../store/storeUser";
 import { Redirect } from "react-router-dom";
 
+function reducer(state, action) {}
+
 function Login() {
   let [username, setUsername] = useState("");
   let [login, setLogin] = useState(false);
+  let [openRegister, updateOpenRegister] = useState(false);
+  let [state, dispatch] = useReducer(reducer, {
+    showModal: false
+  });
 
   function changeUsername(e) {
     setUsername(e.target.value);
@@ -19,25 +25,53 @@ function Login() {
     }
   }, []);
 
+  function onRegister(e) {
+    e.preventDefault();
+    axios.post("/register", { user: username }).then(response => {
+      console.log(response.data);
+    });
+  }
+
   function onSubmit(e) {
     e.preventDefault();
-    axios.post("/login", { user: username }).then(response => {
+    axios.get(`/login/${username}`).then(response => {
       console.log(response);
       updateUser(response.data);
+      if (response.data === username) {
+        updateUserLogin(true);
+        setLogin(true);
+      }
     });
-    updateUserLogin(true);
-    setLogin(true);
   }
 
   return login ? (
     <Redirect to="/" />
   ) : (
     <div className="Login">
+      {openRegister ? (
+        <div>
+          <p>Register</p>
+          <form onSubmit={onRegister}>
+            <p>Username</p>
+            <input onChange={changeUsername} />
+            <button type="submit">Register</button>
+          </form>
+        </div>
+      ) : null}
+
       <p>Login</p>
       <form onSubmit={onSubmit}>
         <input onChange={changeUsername} value={username} required />
-        <button type="submit">Submit</button>
+        <button type="submit">Login</button>
       </form>
+      <button
+        className="Login__register"
+        onClick={() =>
+          !openRegister ? updateOpenRegister(true) : updateOpenRegister(false)
+        }
+      >
+        Register
+      </button>
     </div>
   );
 }
